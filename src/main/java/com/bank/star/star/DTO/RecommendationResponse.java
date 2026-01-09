a5aaf57317b3336e2a06f2515ac17cfa462dfb42
master <aleshenko_ira_2017@mail.ru>
2026-01-09 08:55:01 +0300

    bank

---
+++ src/main/java/com/bank/star/star/DTO/RecommendationResponse.java
@@ -0,0 +1,26 @@
+package com.bank.star.star.DTO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class RecommendationResponse{
+
+    @JsonProperty("user_id")
+    private String userId;
+
+    private List<ProductRecommendation> recommendations;
+
+    public RecommendationResponse(String userId, List<ProductRecommendation> recommendations) {
+        this.userId = userId;
+        this.recommendations = recommendations;
+    }
+
+    public String getUserId() { return userId; }
+    public void setUserId(String userId) { this.userId = userId; }
+
+    public List<ProductRecommendation> getRecommendations() { return recommendations; }
+    public void setRecommendations(List<ProductRecommendation> recommendations) {
+        this.recommendations = recommendations;
+    }
+}
