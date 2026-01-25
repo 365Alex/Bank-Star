499d9bc8c01c431d5d42ab56307d25d0111e40e7
master <aleshenko_ira_2017@mail.ru>
2026-01-25 14:10:23 +0300

    delete 2 classes configuration spring-boot version 3.2.3

--- src/main/java/com/bank/star/star/configuration/RecommendationsDataSourceConfiguration.java
+++ src/main/java/com/bank/star/star/configuration/RecommendationsDataSourceConfiguration.java
@@ -1,13 +1,14 @@
 package com.bank.star.star.configuration;
 
+
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
@@ -15,10 +16,12 @@ import javax.sql.DataSource;
 @Configuration
 public class RecommendationsDataSourceConfiguration {
 
+
     @Bean(name = "recommendationsDataSource")
-    @ConfigurationProperties(prefix = "application.recommendations-db")
-    public DataSource recommendationsDataSource() {
+    public DataSource recommendationsDataSource(@Value("application.recommendations-db.url") String url) {
         HikariDataSource dataSource = DataSourceBuilder.create()
+                .url(url)
+                .driverClassName("org.h2.Driver")
                 .type(HikariDataSource.class)
                 .build();
         dataSource.setReadOnly(true); // Устанавливаем read-only
@@ -29,5 +32,11 @@ public class RecommendationsDataSourceConfiguration {
     public JdbcTemplate recommendationsJdbcTemplate(
             @Qualifier("recommendationsDataSource") DataSource dataSource) {
         return new JdbcTemplate(dataSource);
+    }
+
+    @Primary
+    @Bean(name = "defaultDataSource")
+    public DataSource defaultDataSource(DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().build();
     }
 }
